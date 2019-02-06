10ddedaeb609755b812251340c42a33610348014
Roman Malyshkov <malyshkov.roman@gmail.com>
2019-02-06 14:29:08 +0600

    Webpack updates.

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'development',
   entry: './src/script.js',
   output: {
     filename: 'main.js',
@@ -11,5 +12,10 @@ module.exports = {
       'vue$': 'vue/dist/vue.esm.js' // 'vue/dist/vue.common.js' для webpack 1
     }
   },
-  
+  watch: true,
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000
+  }
 };
